1bf86327a759989d6df4113f849a2ace82cf22ff
David Lower <davidlower4@gmail.com>
2018-01-09 12:58:01 +0000

    add buttons, aside, header and footer. Footer is sticky. Also working towards Jquery to turn on and off the sticky headers and footers

---
+++ main.js
@@ -0,0 +1,12 @@
+$("document").ready(function() {
+   $(".content-wrapper").show();
+
+   $(".btn-4").on("click", function() {
+      // $(".container") removeClass("content.wrapper");
+   });
+
+
+
+
+
+});
